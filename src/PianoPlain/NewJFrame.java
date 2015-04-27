22653dab96d7bf44816a26546b3d85c5967537c1
unknown <iamnowshad@gmail.com>
2015-04-27 20:27:24 +0600

    Modified testSound. Working loadSounds & playSounds function

--- src/PianoPlain/NewJFrame.java
+++ src/PianoPlain/NewJFrame.java
@@ -11,6 +11,7 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class NewJFrame extends javax.swing.JFrame{
 
@@ -61,7 +62,7 @@ public class NewJFrame extends javax.swing.JFrame{
     public void Play(String filename)
 	{
             try{
-                playSound("src\\PianoPlain\\wav\\"+filename);
+                playSounds("src\\PianoPlain\\wav\\"+filename);
             } catch(Exception e){
                 System.out.println("exception in play()");
             }            
@@ -99,8 +100,135 @@ public class NewJFrame extends javax.swing.JFrame{
             }
         }
    }*/
-    
     private final int BUFFER_SIZE = 128000;
+    private File soundFile;
+    private AudioInputStream audioStream;
+    //private AudioFormat audioFormat;
+    private SourceDataLine sourceLine;
+    //private DataLine.Info info;
+    //private ArrayList<AudioFormat> audioFormat;
+    //private ArrayList<DataLine.Info> info;
+    private final AudioFormat[] audioFormat = new AudioFormat[3];
+    private final DataLine.Info[]  info= new DataLine.Info[3];
+    // now Load & play in different functions
+    
+    // loadSounds()
+        //files names are passed
+        //create an array of sourceLine naming each same as the filename
+        //details
+            //an array of filenames are passed as parameter
+            //load all files differently
+    
+    public void LoadSounds(String fileNames[]){
+        for (String strFilename : fileNames) {
+            try {
+                soundFile = new File(strFilename);
+            } catch (Exception e) {
+                System.exit(1);
+            }
+
+            try {
+                audioStream = AudioSystem.getAudioInputStream(soundFile);
+            } catch (UnsupportedAudioFileException | IOException e){
+                System.exit(1);
+            }
+            if(null != strFilename)switch (strFilename) {
+                case "src//wav//a.wav":
+                    audioFormat[0] = audioStream.getFormat();
+                    info[0] = new DataLine.Info(SourceDataLine.class, audioFormat[1]);
+                    System.out.println(audioFormat[0] + " " + info[0]);
+                    break;
+                case "src//wav//b.wav":
+                    audioFormat[1] = audioStream.getFormat();
+                    info[1] = new DataLine.Info(SourceDataLine.class, audioFormat[1]);
+                    System.out.println(audioFormat[0] + " " + info[0]);
+                    break;
+                case "src//wav//c.wav":
+                    audioFormat[2] = audioStream.getFormat();
+                    info[2] = new DataLine.Info(SourceDataLine.class, audioFormat[1]);
+                    System.out.println(audioFormat[0] + " " + info[0]);
+                    break;
+            }
+        }
+        System.out.println("Sound Files Loaded");
+    }
+    // playSounds()
+        //a single filename is received
+        //finds the named file from the array of sourceLine
+        // & play it
+        // then release the memory
+    public void playSounds(String strFilename){
+        try {
+                if(null != strFilename)switch (strFilename) {
+                case "src//wav//a.wav":
+                    sourceLine = (SourceDataLine) AudioSystem.getLine(info[0]);
+                    sourceLine.open(audioFormat[0]);
+                    sourceLine.start();
+                    int nBytesRead1 = 0;
+                    byte[] abData1 = new byte[BUFFER_SIZE];
+                    while (nBytesRead1 != -1) {
+                        try {
+                            nBytesRead1= audioStream.read(abData1, 0, abData1.length);
+                        } catch (IOException e) {
+                        }
+                        if (nBytesRead1 >= 0) {
+                            @SuppressWarnings("unused")
+                            int nBytesWritten = sourceLine.write(abData1, 0, nBytesRead1);
+                        }
+                    }
+
+                    sourceLine.drain();
+                    break;
+                case "src//wav//b.wav":
+                    sourceLine = (SourceDataLine) AudioSystem.getLine(info[1]);
+                    sourceLine.open(audioFormat[1]);
+                    sourceLine.start();
+                    int nBytesRead2 = 0;
+                    byte[] abData2 = new byte[BUFFER_SIZE];
+                    while (nBytesRead2 != -1) {
+                        try {
+                            nBytesRead2 = audioStream.read(abData2, 0, abData2.length);
+                        } catch (IOException e) {
+                        }
+                        if (nBytesRead2 >= 0) {
+                            @SuppressWarnings("unused")
+                            int nBytesWritten = sourceLine.write(abData2, 0, nBytesRead2);
+                        }
+                    }
+
+                    sourceLine.drain();
+                    break;
+                case "src//wav//c.wav":
+                    System.out.println("Playing c");
+                    sourceLine = (SourceDataLine) AudioSystem.getLine(info[2]);
+                    sourceLine.open(audioFormat[2]);
+                    sourceLine.start();
+                    int nBytesRead3 = 0;
+                    byte[] abData3 = new byte[BUFFER_SIZE];
+                    while (nBytesRead3 != -1) {
+                        try {
+                            nBytesRead3 = audioStream.read(abData3, 0, abData3.length);
+                        } catch (IOException e) {
+                        }
+                        if (nBytesRead3 >= 0) {
+                            @SuppressWarnings("unused")
+                            int nBytesWritten = sourceLine.write(abData3, 0, nBytesRead3);
+                        }
+                    }
+
+                    sourceLine.drain();
+                    sourceLine.close();
+                    break;
+            }
+            
+                
+            } catch (LineUnavailableException e) {
+                System.exit(1);
+            } catch (Exception e) {
+                System.exit(1);
+            }
+    }
+    /*private final int BUFFER_SIZE = 128000;
     private File soundFile;
     private AudioInputStream audioStream;
     private AudioFormat audioFormat;
@@ -139,7 +267,7 @@ public class NewJFrame extends javax.swing.JFrame{
         }
 
         sourceLine.start();
-
+        
         int nBytesRead = 0;
         byte[] abData = new byte[BUFFER_SIZE];
         while (nBytesRead != -1) {
@@ -156,7 +284,7 @@ public class NewJFrame extends javax.swing.JFrame{
 
         sourceLine.drain();
         sourceLine.close();
-    }
+    }*/
     
     
     private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
@@ -203,17 +331,13 @@ public class NewJFrame extends javax.swing.JFrame{
             java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+            testSound ts = new testSound();
+            String[] soundList = {"src//wav//a.wav", "src//wav//b.wav", "src//wav//c.wav"};
+            ts.LoadSounds(soundList);
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
             new NewJFrame().setVisible(true);
-            //MakeSound MakeSound = new MakeSound();
-            //MakeSound.playSound();
-            /*try {
-                LoadSound();
-            } catch (IOException ex) {
-                Logger.getLogger(NewJFrame.class.getName()).log(Level.SEVERE, null, ex);
-            }*/
+            
         });
     }
 
--- src/PianoPlain/testSound.java
+++ src/PianoPlain/testSound.java
@@ -1,7 +1,6 @@
 
 package PianoPlain;
 
-import static java.awt.SystemColor.info;
 import java.io.File;
 import java.io.IOException;
 import javax.sound.sampled.AudioFormat;
@@ -10,6 +9,7 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  *
@@ -19,9 +19,13 @@ public class testSound {
     private final int BUFFER_SIZE = 128000;
     private File soundFile;
     private AudioInputStream audioStream;
-    private AudioFormat audioFormat;
+    //private AudioFormat audioFormat;
     private SourceDataLine sourceLine;
-    private DataLine.Info info;
+    //private DataLine.Info info;
+    //private ArrayList<AudioFormat> audioFormat;
+    //private ArrayList<DataLine.Info> info;
+    private final AudioFormat[] audioFormat = new AudioFormat[3];
+    private final DataLine.Info[]  info= new DataLine.Info[3];
     // now Load & play in different functions
     
     // loadSounds()
@@ -36,39 +40,110 @@ public class testSound {
             try {
                 soundFile = new File(strFilename);
             } catch (Exception e) {
-                e.printStackTrace();
                 System.exit(1);
             }
 
             try {
                 audioStream = AudioSystem.getAudioInputStream(soundFile);
-            } catch (Exception e){
-                e.printStackTrace();
+            } catch (UnsupportedAudioFileException | IOException e){
                 System.exit(1);
             }
-
-            audioFormat = audioStream.getFormat();
-
-            info = new DataLine.Info(SourceDataLine.class, audioFormat);
+            if(null != strFilename)switch (strFilename) {
+                case "src//wav//a.wav":
+                    audioFormat[0] = audioStream.getFormat();
+                    info[0] = new DataLine.Info(SourceDataLine.class, audioFormat[1]);
+                    System.out.println(audioFormat[0] + " " + info[0]);
+                    break;
+                case "src//wav//b.wav":
+                    audioFormat[1] = audioStream.getFormat();
+                    info[1] = new DataLine.Info(SourceDataLine.class, audioFormat[1]);
+                    System.out.println(audioFormat[0] + " " + info[0]);
+                    break;
+                case "src//wav//c.wav":
+                    audioFormat[2] = audioStream.getFormat();
+                    info[2] = new DataLine.Info(SourceDataLine.class, audioFormat[1]);
+                    System.out.println(audioFormat[0] + " " + info[0]);
+                    break;
+            }
         }
+        System.out.println("Sound Files Loaded");
     }
     // playSounds()
         //a single filename is received
         //finds the named file from the array of sourceLine
         // & play it
         // then release the memory
-    public void playSounds(String filename){
+    public void playSounds(String strFilename){
         try {
-                sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-                sourceLine.open(audioFormat);
+                if(null != strFilename)switch (strFilename) {
+                case "src//wav//a.wav":
+                    sourceLine = (SourceDataLine) AudioSystem.getLine(info[0]);
+                    sourceLine.open(audioFormat[0]);
+                    sourceLine.start();
+                    int nBytesRead1 = 0;
+                    byte[] abData1 = new byte[BUFFER_SIZE];
+                    while (nBytesRead1 != -1) {
+                        try {
+                            nBytesRead1= audioStream.read(abData1, 0, abData1.length);
+                        } catch (IOException e) {
+                        }
+                        if (nBytesRead1 >= 0) {
+                            @SuppressWarnings("unused")
+                            int nBytesWritten = sourceLine.write(abData1, 0, nBytesRead1);
+                        }
+                    }
+
+                    sourceLine.drain();
+                    break;
+                case "src//wav//b.wav":
+                    sourceLine = (SourceDataLine) AudioSystem.getLine(info[1]);
+                    sourceLine.open(audioFormat[1]);
+                    sourceLine.start();
+                    int nBytesRead2 = 0;
+                    byte[] abData2 = new byte[BUFFER_SIZE];
+                    while (nBytesRead2 != -1) {
+                        try {
+                            nBytesRead2 = audioStream.read(abData2, 0, abData2.length);
+                        } catch (IOException e) {
+                        }
+                        if (nBytesRead2 >= 0) {
+                            @SuppressWarnings("unused")
+                            int nBytesWritten = sourceLine.write(abData2, 0, nBytesRead2);
+                        }
+                    }
+
+                    sourceLine.drain();
+                    break;
+                case "src//wav//c.wav":
+                    System.out.println("Playing c");
+                    sourceLine = (SourceDataLine) AudioSystem.getLine(info[2]);
+                    sourceLine.open(audioFormat[2]);
+                    sourceLine.start();
+                    int nBytesRead3 = 0;
+                    byte[] abData3 = new byte[BUFFER_SIZE];
+                    while (nBytesRead3 != -1) {
+                        try {
+                            nBytesRead3 = audioStream.read(abData3, 0, abData3.length);
+                        } catch (IOException e) {
+                        }
+                        if (nBytesRead3 >= 0) {
+                            @SuppressWarnings("unused")
+                            int nBytesWritten = sourceLine.write(abData3, 0, nBytesRead3);
+                        }
+                    }
+
+                    sourceLine.drain();
+                    break;
+            }
+            
+                
             } catch (LineUnavailableException e) {
-                e.printStackTrace();
                 System.exit(1);
             } catch (Exception e) {
-                e.printStackTrace();
                 System.exit(1);
             }
     }
+    /*
     public void playSound(String filename){
 
         String strFilename = filename;
@@ -76,14 +151,12 @@ public class testSound {
         try {
             soundFile = new File(strFilename);
         } catch (Exception e) {
-            e.printStackTrace();
             System.exit(1);
         }
 
         try {
             audioStream = AudioSystem.getAudioInputStream(soundFile);
-        } catch (Exception e){
-            e.printStackTrace();
+        } catch (UnsupportedAudioFileException | IOException e){
             System.exit(1);
         }
 
@@ -120,10 +193,12 @@ public class testSound {
         sourceLine.drain();
         sourceLine.close();
     }
-    
+    */
     public static void main(String args[]){
         testSound ts = new testSound();
-        ts.playSound("src//wav//a.wav");
+        String[] soundList = {"src//wav//a.wav", "src//wav//b.wav", "src//wav//c.wav"};
+        ts.LoadSounds(soundList);
+        ts.playSounds("src//wav//c.wav");
     }
     
 }
