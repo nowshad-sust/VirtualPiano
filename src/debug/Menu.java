6e156722eaa771ac335eef8da8777e0b16be025f
nowshad_sust <iamnowshad@gmail.com>
2015-07-26 21:55:20 +0600

    added javadoc for this class

--- src/debug/Menu.java
+++ src/debug/Menu.java
@@ -15,32 +15,14 @@
  */
 
 
-/**
- * Class to create the menu shown at the top Panel of the main GUI
- * createManu is a function that creates a tabbed menu (which is now commented out)
- * createDifferentMenu is the currently working menu
- * works fine with one bug
- * 
- * BUG:
- *      Location: menuLabel
- *      summary: when an string is assigned to the label
- *               it generates a mess on that label
- *      primary solution: made the whole menu background black
- *                        that hides the bug happening
- *      reason: still unknown
-*/
+
 package debug;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Desktop;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Insets;
-import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -48,106 +30,59 @@ import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 
 /**
  *
  * @author nowshad
  */
 
+/**
+ * Class to create the menu shown at the top Panel of the main GUI
+ * 
+ */
 public class Menu extends JFrame implements ActionListener{
+    //declaring static labels & variable
     public static JLabel currentlyPlayingLabel  = new JLabel();
     public static JLabel menuLabel = new JLabel("Main Menu");;
     public static String currentlyPlayingNote = "Nothing";
-    public Menu(){
-        /*
-        JFrame frame = new JFrame();
-        //frame.setContentPane(new JLabel(new ImageIcon("images/background.jpg")));
-        
-        JPanel topPanel = createMenu();  
-        
-        
-        frame.add(topPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setSize(900,400);
-       */
-    }
-    /*
-    public static JPanel createMenu(){
-        
-        JPanel topPanel = new JPanel();
-        topPanel.setBackground(new Color(0,0,0,0));
-        
-        JTabbedPane tabbedMenuPanel = new JTabbedPane();
-        //tabbedMenuPanel.setSize(200, 100);
-        tabbedMenuPanel.setBackground(new Color(0,0,0,0));
-        tabbedMenuPanel.setBorder(null);
-        tabbedMenuPanel.setFocusable(false);
-        
-        //1st tab -> CurrentlyPlayingPanel
-        
-        JPanel CurrentlyPlayingPanel = new JPanel();
-        
-        CurrentlyPlayingPanel.setLayout(new BoxLayout(CurrentlyPlayingPanel, BoxLayout.Y_AXIS));
-        
-        CurrentlyPlayingPanel.setBackground(new Color(0,0,0,0));
-        
-        JLabel label1 = new JLabel();
-        label1.setText("<html><h3>Currently Playing Note</h3></html>");
-        currentlyPlayingLabel = new JLabel("<html><h3>Nothing</h3></html>");
-        CurrentlyPlayingPanel.add(label1);
-        CurrentlyPlayingPanel.add(currentlyPlayingLabel);
-        
-        //2nd tab -> CreditsPanel
-        JPanel creditsPanel = new JPanel();
-        creditsPanel.setBackground(new Color(0,0,0,0));
-        JLabel creditsLabel = new JLabel();
-        //credits text
-        creditsLabel.setText("<html>Md. Al-amin Nowshad"
-                + "<br>Batch: 2012,"
-                + "<br>Dept. of Computer Science & Engineering,"
-                + "<br>ShahJalal University of Science & Technology"
-                + "<br>email: iamnowshad@gmail.com</html>");
-        
-        creditsPanel.add(creditsLabel);
-        
-        //3rd tab -> helpPanel
-        JPanel helpPanel = new JPanel();
-        helpPanel.setBackground(new Color(0,0,0,0));
-        JLabel helpLabel = new JLabel("<html><p>Watch the keymap to play</p></html>");
-        JLabel linkLabel = new JLabel();
-        PianoFunctionality.goWebsite(linkLabel,"http://nowshad.scdnlab.com","Click here");
-        helpPanel.add(helpLabel);
-        helpPanel.add(linkLabel);
-        
-        //adding tabs to main JTabbedPane
-        tabbedMenuPanel.addTab("Playing", CurrentlyPlayingPanel);
-        tabbedMenuPanel.addTab("Credits", creditsPanel);
-        tabbedMenuPanel.addTab("Help", helpPanel);
-        
-        topPanel.add(tabbedMenuPanel);
-        
-        return topPanel;
-        
-    }
-    */
     
-    //function to create menu with four button like tabbed menu
+    /**
+     * Empty constructor
+     */
+    public Menu(){
+        
+    }
+    
+    /**
+     * function to create menu with four button(like tabbed menu)
+     * This function creates a menu which will be added to the
+     * main Frame of the application at the top bar.
+     * <p>
+     * The menu is divided into two parts -
+     * <dl>
+     *   <dt>Menu Tabs</dt>
+     *     <dd>- the tabs to navigate display contents</dd>
+     *   <dt>Display Area</dt>
+     *     <dd>- display the contents by the selected tab</dd>
+     *  </dl>
+     * 
+     * @return <code>differentMenu</code> which is the new menu
+     *              that is created in a <code>JPanel</code>.
+     * @see JPanel
+     * @see #goWebsite(javax.swing.JButton, java.lang.String)
+     * @see #setButtonDesign(javax.swing.JButton) 
+     */
     public JPanel createDifferentMenu(){
         
         JPanel differentMenu = new JPanel();
         differentMenu.setBackground(new Color(0,0,0,65));
         //differentMenu.setBackground(Color.black);
         
-        //Menu bar just four buttons
+        //Menu bar with just four buttons
         JPanel menuBar = new JPanel();
         menuBar.setLayout(new GridLayout(1,4));
         
@@ -172,13 +107,7 @@ public class Menu extends JFrame implements ActionListener{
         //menu contents display area
         JPanel menuPanel = new JPanel();
         menuPanel.setBackground(Color.black);
-        
-        /*
-        GUI bug:
-            if the background is transparent then bug happens
-            //menuPanel.setBackground(new Color(0,0,0,65));
-        */
-        
+
         menuPanel.setLayout(new FlowLayout());
         
         //menuLabel.setBackground(Color.BLACK);
@@ -194,7 +123,31 @@ public class Menu extends JFrame implements ActionListener{
         return differentMenu;
        
     }
-    //mouse event handler
+    
+    /**
+     * function to handle the mouse events on the tabs
+     * Checks which tabbed is clicked as well as changes 
+     * the display area contents
+     * <p>
+     * there are four types of response based on which
+     * tab is selected-
+     *  <dl>
+     *   <dt>Playing</dt>
+     *     <dd>- just shows the currently playing note</dd>
+     *   <dt>Manual</dt>
+     *     <dd>- opens a new window which describes the
+     *          <br>procedures of using this application
+     *     </dd>
+     *   <dt>Credits</dt>
+     *     <dd>- shows the credits and info of the developer</dd>
+     *   <dt>Help</dt>
+     *     <dd>- takes to the help website</dd>
+     *  </dl> 
+     * 
+     * 
+     * @see ManualClass
+     * @param e just the action event which was not used
+     */
 	public void actionPerformed(ActionEvent e)
         {
             if(e.getActionCommand().equals("Playing"))
@@ -215,8 +168,6 @@ public class Menu extends JFrame implements ActionListener{
             }
             else if(e.getActionCommand().equals("Manual"))
             {
-                String manualText = "<html>Manual Window</html>";
-                //menuLabel.setText(manualText);
                 ManualClass manualObject = new ManualClass();
                 JFrame manualFrame = manualObject.createManual();
                 manualFrame.setVisible(true);
@@ -224,9 +175,7 @@ public class Menu extends JFrame implements ActionListener{
             }
             else if(e.getActionCommand().equals("Help"))
             {
-                //PianoFunctionality.goWebsite(menuLabel,"http://nowshad.scdnlab.com","Click here");
                 String helpText = "<html><b>Help Menu<b></html>";
-                //menuLabel.setText(helpText);
             }
             else{
                 String playingText = "<html><h2>Currently Playing Note</h2>"
@@ -234,7 +183,15 @@ public class Menu extends JFrame implements ActionListener{
                 menuLabel.setText(playingText);
             }
         }
-    
+    /**
+     * function that sets the style for the tab buttons
+     * 
+     * makes background transparent
+     * adds event handler
+     * makes non-focusable
+     * 
+     * @param jb a JButton object
+     */
     public void setButtonDesign(JButton jb){
                         String name  = jb.getText();
                         jb.setContentAreaFilled(false);
@@ -247,18 +204,15 @@ public class Menu extends JFrame implements ActionListener{
                         //jb.setPressedIcon(new ImageIcon("images/blackKey.png"));
     }
     
-
-    public void setCurrenttlyPlayingNote(String note) {
-        this.currentlyPlayingNote = note;
-        String playingText = "<html><h2>Currently Playing Note</h2>"
-                + "<br>"+note+"</html>";
-        try{
-            menuLabel.setText(playingText);
-        }catch(Exception e){
-            System.out.println("label overriding failed");
-        }
-        
-    }
+    /**
+     * function that makes a button clickable and sends to an website
+     * 
+     * adds a mouseListener to the button
+     * takes to the help website
+     * 
+     * @param website a button
+     * @param url address of the website
+     */
      public static void goWebsite(JButton website, final String url) {
         //website.setText("<html> : <a href=\"\">"+text+"</a></html>");
         website.setCursor(new Cursor(Cursor.HAND_CURSOR));
